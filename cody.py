60c632d0583c1420a1092d3facc2da3ef620d966
Benexelus <It_bene@gmx.de>
2026-03-17 15:35:24 +0100

    Update cody.py

--- cody.py
+++ cody.py
@@ -27,7 +27,7 @@ np.set_printoptions(suppress=True)
 # Supabase Verbindung
 # ------------------------
 SUPABASE_URL = "https://....supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdiYnd6ZXVodGp4eGppeXprcGlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NTU5ODYsImV4cCI6MjA4ODAzMTk4Nn0.IuaU1dd1_Xu7ZTd5l2FEdUSBigOWoLOky7h4HhAA_JE"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..IuaU1dd1_Xu7ZTd5l2FEdUSBigOWoLOky7h4HhAA_JE"
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # ------------------------
